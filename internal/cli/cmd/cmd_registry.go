5b06c6b70ceccc3004b9fa83d1e594892a4028e5
berrythewa <waberryd@proton.me>
2025-03-24 17:47:39 +0100

    daemonization -WIP 2

---
+++ internal/cli/cmd/cmd_registry.go
@@ -0,0 +1,16 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// GetCommands returns all commands for registration
+func GetCommands() []*cobra.Command {
+	return []*cobra.Command{
+		versionCmd,
+		runCmd,
+		historyCmd,
+		flushCmd,
+		serviceCmd,
+	}
+} 
